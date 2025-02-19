60ba7d2575d746f9c4d57a7ece4ca229ef7605b7
Saminda Fernando <uthpala2001@gmail.com>
2025-02-19 12:08:16 +0530

    vehicle done

---
+++ model/Vehicle.ts
@@ -0,0 +1,13 @@
+export default class Vehicle {
+    vehicle_code!:string
+    email!:string
+    first_name!:string
+    fuel_type!:string
+    license_plate!:string
+    phone_no!:string
+    remarks!:string
+    role!:string
+    status!:string
+    vehicle_category!:string
+    staff_id!:string
+}
