18638d8a9fafc1ea7c73b7a682bd66b7f79543f0
Saminda Fernando <uthpala2001@gmail.com>
2025-02-19 12:08:06 +0530

    staff log done

---
+++ model/StaffLogs.ts
@@ -0,0 +1,9 @@
+export default class StaffLogs {
+    log_code!:string
+    details!:string
+    img!:string
+    log_date!:string
+    first_name!:string
+    phone_no!:string
+    staff_id!:string
+}
