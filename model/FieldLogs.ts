11b4a20c9da8b4f3b3b63d98c111b41fc8c547fe
Saminda Fernando <uthpala2001@gmail.com>
2025-02-19 12:07:42 +0530

    field logs done

---
+++ model/FieldLogs.ts
@@ -0,0 +1,9 @@
+export default class FieldLogs {
+    log_code!:string
+    details!:string
+    img!:string
+    log_date!:string
+    field_location!:string
+    field_name!:string
+    field_code!:string
+}
